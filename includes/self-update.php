10f45d83fca9974ccb02a9f0ea25006c1d17ce3a
Nick <nick.green@automattic.com>
2024-04-01 15:15:58 -0700

    Fix docblock
    Co-authored-by: Fernando Fernandes <fernandofernand3s@gmail.com>

--- includes/self-update.php
+++ includes/self-update.php
@@ -4,12 +4,12 @@ add_filter( 'update_plugins_github.com', 'feedland_blogroll_self_update', 10, 4 
 /**
  * Check for updates to this plugin
  *
- * @param array  $update   Array of update data.
- * @param array  $plugin_data Array of plugin data.
- * @param string $plugin_file Path to plugin file.
- * @param string $locales    Locale code.
+ * @param array|false $update      Array of update data.
+ * @param array       $plugin_data Array of plugin data.
+ * @param string      $plugin_file Path to plugin file.
+ * @param string[]    $locales     Locale code.
  *
- * @return array|bool Array of update data or false if no update available.
+ * @return array|false Array of update data or false if no update available.
  */
 function feedland_blogroll_self_update( $update, array $plugin_data, string $plugin_file, $locales ) {
 
